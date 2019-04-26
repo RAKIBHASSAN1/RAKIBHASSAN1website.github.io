ea77c0ec108484d427f263fcfc0a2bf836d935eb
RAKIBHASSAN1 <47098282+RAKIBHASSAN1@users.noreply.github.com>
2019-04-26 13:03:09 +0600

    Add files via upload

---
+++ after_adminsays.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Sell Bye & Exchange </title>
+    <link rel="icon" type="image/ico" href="images/tbl.jpg" />
+    <link href="styles/after_admin_say.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div id="v">
+        <br><br>
+        <p id="n">Admin tell about website</p><br>
+        <p id="n1">Dear User You can see sell product use search key what product sell  in nowadays but be carefull about
+        searching key beacause it can only work when you search product type or product id. Another mehood see selling product information Buy product manu.
+        Any problem face payment methood and know user master card or other methood information you can contact us anytime.</p>
+        <p id="n2">Happy shopping user</p>
+    </div>
+    <div id="v1">
+        <a href="after_login.php">Back Home</a>
+    </div>
+
+</body>
+
+</html>
